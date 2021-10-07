a8cd1b8c192969b136144504a7fe55643794003f
Lucas Brito <lucasbsilva29@gmail.com>
2021-10-06 23:10:58 -0300

    style(main.py): indentation

--- src/main.py
+++ src/main.py
@@ -4,23 +4,29 @@ import numpy as np
 
 def minha_funcao(dados: pd.DataFrame, status_resposta: str) -> pd.DataFrame:
     """ # Minha Função
-    Função para calculo de grupo, quantidade de registros de cada grupo e a taxa de resposta.
+    Função para calculo de grupo, quantidade de registros de cada
+     grupo e a taxa de resposta.
 
     Args:
-        dados (pd.DataFrame): Dataframe com grupos e resposta se foi contratado ou não;
+        dados (pd.DataFrame): Dataframe com grupos e resposta se
+         foi contratado ou não;
         status_resposta (str): String para filtro de contratação.
 
     Raises:
-        TypeError: Erro devido a inserção de valor que não é string no segundo parâmetro.
+        TypeError: Erro devido a inserção de valor que não é string
+         no segundo parâmetro.
 
     Returns:
-        pd.DataFrame: Dataframe com grupos, quantidade e taxa de resposta calculada.
+        pd.DataFrame: Dataframe com grupos, quantidade e taxa de
+         resposta calculada.
     """
     if (not isinstance(status_resposta, str)):
         raise TypeError('invalid input, try to send a string with "S" or "N".')
+
     df = dados.groupby('covariavel').count()
     df['taxa_resposta'] = (dados[(dados.contratou == status_resposta)].groupby(
-        'covariavel').count() / dados.groupby('covariavel').count()).contratou.to_list()
+        'covariavel'
+    ).count() / dados.groupby('covariavel').count()).contratou.to_list()
     df = df.rename(columns={"contratou": "n"})
     return df.reset_index()
 
